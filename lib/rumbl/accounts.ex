a66275dfc8ed3e7eb166d89d086a0cd89b4b378c
Majid Qureshi <mpq1990@gmail.com>
2023-01-20 12:53:54 +0100

    feat: add user authentication

--- lib/rumbl/accounts.ex
+++ lib/rumbl/accounts.ex
@@ -10,6 +10,10 @@ defmodule Rumbl.Accounts do
     Repo.get(User, id)
   end
 
+  def get_user(id) do
+    Repo.get(User, id)
+  end
+
   def get_user_by(params) do
     Repo.get_by(User, params)
   end
@@ -36,5 +40,21 @@ defmodule Rumbl.Accounts do
     %User{}
     |> User.registration_changeset(attrs)
     |> Repo.insert()
+  end
+
+  def authenticate_by_username_and_pass(username, given_pass) do
+    user = get_user_by(username: username)
+
+    cond do
+      user && Pbkdf2.verify_pass(given_pass, user.password_hash) ->
+        {:ok, user}
+
+      user ->
+        {:error, :unauthorized}
+
+      true ->
+        Pbkdf2.no_user_verify()
+        {:error, :not_found}
+    end
   end
 end
